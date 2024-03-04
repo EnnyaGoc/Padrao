d21b3879361fa16ee1d508d4472fdd03e19a8e87
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-03-04 13:41:33 -0300

    .

--- facade/main.py
+++ facade/main.py
@@ -7,7 +7,7 @@ class main:
     reserva.reservarViagem("2024-05-07", "eua", "algumhotel", "14", "porsche", "454653767566756", 15000.00)
 
 
-    #o padroa Facade facilita a intereçao do cliente pois
+    #o padrao Facade facilita a intereçao do cliente pois,
     #nesse caso, fornece uma interface simples e unificada para 
     #as reservas e o pagamento.
     #o cliente só interage com a classe Facade
