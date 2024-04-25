495b306b371a1462824b42f492ca9666f536e2d1
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-04-25 11:02:30 -0300

    .

---
+++ prototype/Cliente.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cliente {
+    public static void main(String[] args) {
+        Inimigo prototipo1 = new Inimigo("Goblin", 50, 10);
+        Inimigo prototipo2 = new Inimigo("Esqueleto", 40, 15);
+
+        Map<String, Inimigo> inimigos = new HashMap<>();
+
+        for (int i = 1; i <= 5; i++) {
+            Inimigo novoInimigo = prototipo1.clone();
+            novoInimigo.setVida(novoInimigo.getVida() + i * 10);
+            inimigos.put("Goblin " + i, novoInimigo);
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            Inimigo novoInimigo = prototipo2.clone();
+            novoInimigo.setAtaque(novoInimigo.getAtaque() + i * 5);
+            inimigos.put("Esqueleto " + i, novoInimigo);
+        }
+
+        for (Map.Entry<String, Inimigo> entry : inimigos.entrySet()) {
+            String nome = entry.getKey();
+            Inimigo inimigo = entry.getValue();
+            System.out.println(nome + ": Tipo - " + inimigo.getTipo() + ", Vida - " + inimigo.getVida() + ", Ataque - " + inimigo.getAtaque());
+        }
+    }
+}
---
+++ prototype/Inimigo.java
@@ -0,0 +1,39 @@
+public class Inimigo implements Cloneable {
+    private String tipo;
+    private int vida;
+    private int ataque;
+
+    public Inimigo(String tipo, int vida, int ataque) {
+        this.tipo = tipo;
+        this.vida = vida;
+        this.ataque = ataque;
+    }
+
+    public Inimigo clone() {
+        try {
+            return (Inimigo) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return null;
+        }
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+}
